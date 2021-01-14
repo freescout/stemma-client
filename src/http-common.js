e3d091075434ceeed69bdeb063687df1ac7d3447
freescout <neethu.jacob@gmail.com>
2021-01-14 14:10:50 +0100

    baseURL

--- src/http-common.js
+++ src/http-common.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 console.log("Base url", process.env);
 export default axios.create({
   
-  baseUrl : "https://stemma.herokuapp.com/api" || "http://localhost:8080/api",
+  baseURL : "https://stemma.herokuapp.com/api" || "http://localhost:8080/api",
   headers: {
     "Content-type": "application/json"
   }
